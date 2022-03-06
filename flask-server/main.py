6a70073318085bbf10451ed1d8beefeb95fea646
nataliewutc <nataliewu.261201@gmail.com>
2022-03-06 10:26:32 +0000

    reddit scraper

--- flask-server/main.py
+++ flask-server/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
+import csv
+import time
 
 app = Flask("__main__")
 
@@ -14,13 +16,23 @@ def get_youtube_videos(title):
     videoImage = thumbSoupMeta["content"] if thumbSoupMeta else "Notfound"
     return(videoTitle, "Thumbnail", videoImage) # ttps://i.ytimg.com/vi/rfscVS0vtbw/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG\u0026rs=AOn4CLBcbOHe1sGObiBHwNSA32uZ25dPgg
 
+def get_reddit_posts(title):
+    url = "https://www.reddit.com/search/?q=how%20to%20make%20a%20web%20app%20with%20python"
+    hdr = { 'User-Agent' : 'Nomads shower crawler'}
+    req = urllib2.Request(url, headers=hdr)
+    htmlpage = urlib2.urlopen(req).read()
+    BeautifulSoupFormat = Beautifulsoup(htmlpage, 'lxml')
+    name_box = BeautifulSoupFormat.find_all('a')
+    return(data.text)
+
 @app.route("/")
 def my_index():
     return render_template("index.html")
 
-@app.route('/api/get_youtube', methods=["POST"])
-def get_playlist():
-    get_youtube_videos(sentence)
-    return jsonify({"playlist_id": playlist_id})
+@app.route('/api/get_resources', methods=["POST"])
+def get_lists():
+    keys = get_youtube_videos(sentence) + get_reddit-posts(sentence)
+    list_of_resources = dict(zip(keys, values))
+    return jsonify({"list_id": list_id})
 
 app.run(debug=True)
